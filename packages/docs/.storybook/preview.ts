c7fb17e9458eb46e2d58f5cf1aff324ce01bd6ad
Mateus <mateushnr.obs@gmail.com>
2024-05-31 15:33:13 -0300

    fix: remove theming from preview

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -1,9 +1,7 @@
 import type { Preview } from "@storybook/react";
-import { themes } from '@storybook/theming'
 
 const preview: Preview = {
   parameters: {
-    theme: themes.dark,
     controls: {
       matchers: {
         color: /(background|color)$/i,
